900d64aecf53e147114c84a86c3bab1118fd47c9
Eleanor Jackson <eleanor.elizabeth.j@gmail.com>
2019-10-30 15:28:07 +0000

    Update DataWrang.R

--- Week3/code/DataWrang.R
+++ Week3/code/DataWrang.R
@@ -5,6 +5,7 @@
 ############# Load the dataset ###############
 # header = false because the raw data don't have real headers
 MyData <- as.matrix(read.csv("../data/PoundHillData.csv",header = F)) 
+# Loading the data as.matrix(), and setting the header and stringsAsFactors guarantees that the data are imported "as is" so that you can wrangle them. Otherwise read.csv will convert the first row to column headers, convert everything to factors 
 
 # header = true because we do have metadata headers
 MyMetaData <- read.csv("../data/PoundHillMetaData.csv",header = T, sep=";", stringsAsFactors = F)
@@ -27,7 +28,8 @@ MyData[MyData == ""] = 0
 
 ############# Convert raw matrix to data frame ###############
 
-TempData <- as.data.frame(MyData[-1,],stringsAsFactors = F) #stringsAsFactors = F is important!
+TempData <- as.data.frame(MyData[-1,],stringsAsFactors = F) #stringsAsFactors = F is important! Deleting column names
+
 colnames(TempData) <- MyData[1,] # assign column names from original data
 
 ############# Convert from wide to long format  ###############
